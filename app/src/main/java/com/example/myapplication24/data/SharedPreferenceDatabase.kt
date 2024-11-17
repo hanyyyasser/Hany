95e0b42914662857dc4e82ce33e7a0084e409df6
hany <hany.yasser2007@gmail.com>
2024-11-17 21:09:20 +0200

    ui edit

---
+++ app/src/main/java/com/example/myapplication24/data/SharedPreferenceDatabase.kt
@@ -0,0 +1,57 @@
+package com.example.myapplication24.data
+
+import android.content.Context
+import android.content.SharedPreferences
+
+object SharedPreferenceDatabase {
+    private var appContext: Context? = null
+    private const val SHARED_PREFERENCES_NAME_KEY = "User data"
+    private const val USER_NAME_KEY = "name"
+    private const val USER_NUMBER_OF_CHILDREN = "number of children"
+    private const val USER_Address = "adress"
+
+    fun initSharedPrefDatabase(context: Context?) {
+        appContext = context
+    }
+
+    private fun getSharedPreference(): SharedPreferences {
+        return appContext!!.getSharedPreferences(SHARED_PREFERENCES_NAME_KEY, Context.MODE_PRIVATE)
+    }
+
+    private fun setPreference(key: String, value: String) {
+        val editor = getSharedPreference().edit()
+        editor.putString(key,value)
+        editor.apply()
+    }
+
+    private fun getPreference(key: String, defaultValue: String): String {
+        return getSharedPreference().getString(key, defaultValue)!!
+    }
+
+    fun setname(accessToken: String) {
+        setPreference(USER_NAME_KEY, accessToken)
+    }
+
+    fun getname(): String {
+        return getPreference(USER_NAME_KEY, "default access token")
+    }
+
+    fun setAddress(address: String) {
+        setPreference(USER_Address, address)
+    }
+
+    fun getAddress(): String {
+        return getPreference(USER_Address, "default address")
+    }
+
+    fun setnoOfChildren(birthday: String) {
+        setPreference(USER_NUMBER_OF_CHILDREN, birthday)
+    }
+
+    fun getnoOfChildren(): String {
+        return getPreference(USER_NUMBER_OF_CHILDREN, "default birthday")
+    }
+
+
+
+}
