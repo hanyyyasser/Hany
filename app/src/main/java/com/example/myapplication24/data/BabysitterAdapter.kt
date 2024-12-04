e850af961af39cc110c06899ebef65aa1d1d55b7
hany <hany.yasser2007@gmail.com>
2024-12-04 23:31:59 +0200

    commit

--- app/src/main/java/com/example/myapplication24/data/BabysitterAdapter.kt
+++ app/src/main/java/com/example/myapplication24/data/BabysitterAdapter.kt
@@ -10,7 +10,9 @@ import com.example.myapplication24.R
 import com.example.myapplication24.data.Babysitter
 import com.example.myapplication24.data.BabysitterAdapter
 
-class BabysitterAdapter(private val babysitters: List<Babysitter>) : RecyclerView.Adapter<BabysitterAdapter.BabysitterViewHolder>() {
+class BabysitterAdapter(private val babysitters: List<Babysitter>,) : RecyclerView.Adapter<BabysitterAdapter.BabysitterViewHolder>() {
+
+    var onUserClick : OnUserClick? = null //activate objects in recyclerView
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BabysitterViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.view_holder, parent, false)
         return BabysitterViewHolder(itemView)
@@ -31,7 +33,16 @@ class BabysitterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     val imageView : ImageView = itemView.findViewById(R.id.img)
     val nameView : TextView = itemView.findViewById(R.id.nameTxt)
     val priceView : TextView = itemView.findViewById(R.id.costTxt)
+    init {
+        itemView.setOnClickListener {
+            onUserClick?.onClick(babysitters[adapterPosition])//activate objects in recyclerView
+        }
+
 }
 
 
+}
+    interface OnUserClick {
+        fun onClick(babySitter : Babysitter)//activate objects in recyclerView
+    }
 }
